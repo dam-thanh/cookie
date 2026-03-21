abd6c64f2e62f82e964aef769d678e7a32431bb6
Đàm Thanh <112970027+dam-thanh@users.noreply.github.com>
2026-03-21 07:35:44 -0700

    Update button.js

--- button.js
+++ button.js
@@ -1,73 +1,17 @@
-(function () {
-    'use strict';
-
-    function createButton() {
-        if (document.getElementById("autoUptolinkBtn")) return;
-
-        const btn = document.createElement("button");
-        btn.id = "autoUptolinkBtn";
-        btn.innerText = "Chạy Uptolink 2 Step";
-
-        btn.style.position = "fixed";
-        btn.style.bottom = "20px";
-        btn.style.right = "20px";
-        btn.style.zIndex = "9999";
-        btn.style.padding = "10px 15px";
-        btn.style.background = "#10b981";
-        btn.style.color = "white";
-        btn.style.border = "none";
-        btn.style.borderRadius = "8px";
-        btn.style.cursor = "pointer";
-        btn.style.fontWeight = "bold";
-
-        btn.onclick = function () {
-            window.location.href = "https://maxtask.net/home/tasks";
-        };
-
-        document.body.appendChild(btn);
+{
+  "active": "link1",
+  "links": {
+    "link1": {
+      "url": "https://uptolink.one/sadsa",
+      "enabled": true
+    },
+    "link2": {
+      "url": "https://uptolink.one/MzLG",
+      "enabled": false
+    },
+    "link3": {
+      "url": "https://uptolink.one/thanh",
+      "enabled": false
     }
-
-    function autoClickTask() {
-        const titles = document.querySelectorAll("h3");
-
-        titles.forEach(title => {
-            if (title.innerText.trim() === "Uptolink 2 Step") {
-
-                const card = title.closest("div.p-4");
-                if (!card) return;
-
-                const button = card.querySelector("button");
-
-                if (button && !button.disabled) {
-                    console.log("Đã tìm thấy Uptolink 2 Step → Đang bấm");
-                    button.click();
-                }
-            }
-        });
-    }
-
-    window.addEventListener("load", function () {
-
-        createButton();
-
-        if (window.location.href.includes("/home/tasks")) {
-
-            const interval = setInterval(() => {
-                autoClickTask();
-            }, 2000);
-
-            setTimeout(() => clearInterval(interval), 20000);
-        }
-    });
-window.addEventListener("load", function () {
-    createButton();
-
-    if (window.location.href.includes("/home/tasks")) {
-        const interval = setInterval(() => {
-            autoClickTask();
-        }, 2000);
-
-        setTimeout(() => clearInterval(interval), 20000);
-    }
-});
-})();
+  }
+}
